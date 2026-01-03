aa6668dda319d769ddc595bba139a430ee4ce720
katerina <kpavlovaaa@yandex.ru>
2026-01-04 01:49:46 +0300

    add run function and fix path of dependence app

--- backend/gateway/cinema_api_gateway/__init__.py
+++ backend/gateway/cinema_api_gateway/__init__.py
@@ -1,6 +1,11 @@
 import uvicorn
-from main import app
+
+from cinema_api_gateway.main import app
+
+
+def run() -> None:
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # noqa: S104
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    run()
